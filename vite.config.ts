f8b02e63ebafdb394c3a94c22bf83b177031c615
Rizwan Shah <61790197+rizwanshah9792@users.noreply.github.com>
2025-05-27 11:15:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/CO-G/",
   server: {
     host: "::",
     port: 8080
